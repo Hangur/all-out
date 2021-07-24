c61179c24d8dda68be6610afb826efc06ea291d3
Hangur <44006666+Hangur@users.noreply.github.com>
2021-07-24 21:26:02 +0200

    Added API call

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
+const authKey = <put your authKey here>;
 const submitButton = document.getElementById('submit');
-const onSubmit = () => {
+
+const getTranslation = async (authKey, input, sourceLang, targetLang) => {
+    const response = await fetch(`https://api-free.deepl.com/v2/translate` +
+            `?auth_key=${encodeURIComponent(authKey)}` + 
+            `&text=${encodeURIComponent(input)}` + 
+            `&source_lang=${encodeURIComponent(sourceLang)}` + 
+            `&target_lang=${encodeURIComponent(targetLang)}`, {
+        method: 'POST'
+    }).then(async (response) => {
+        if (response.status === 200) {
+            const json = await response.json();
+            return json.translations[0].text;
+        } else {
+            throw new Error("AAAAAAAAAAAAAAA");
+        }
+    }).catch((error) => {
+        console.error(error);
+
+    });
+    
+    return response;
+  };
+
+const onSubmit = async () => {
     const input = document.getElementById('input').value;
-    const fromLanguage = document.getElementById('from-language').value;
-    const toLanguage = document.getElementById('to-language').value;
-    console.log('The value of the input is', input, fromLanguage, toLanguage);
+    const sourceLang = document.getElementById('source-language').value;
+    const targetLang = document.getElementById('target-language').value;
+    const result = await getTranslation(authKey, input, sourceLang, targetLang);
+    console.log(result);
 };
 
 submitButton.addEventListener('click', onSubmit);
+
