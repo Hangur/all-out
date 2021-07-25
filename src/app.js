7de4f182bc81fcd1142e7c1e44701a913c95610a
Hangur <44006666+Hangur@users.noreply.github.com>
2021-07-25 11:43:06 +0200

    Added output value

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const authKey = <put your authKey here>;
 const submitButton = document.getElementById('submit');
 
 const getTranslation = async (authKey, input, sourceLang, targetLang) => {
-    const response = await fetch(`https://api-free.deepl.com/v2/translate` +
+    return await fetch(`https://api-free.deepl.com/v2/translate` +
             `?auth_key=${encodeURIComponent(authKey)}` + 
             `&text=${encodeURIComponent(input)}` + 
             `&source_lang=${encodeURIComponent(sourceLang)}` + 
@@ -13,14 +13,11 @@ const getTranslation = async (authKey, input, sourceLang, targetLang) => {
             const json = await response.json();
             return json.translations[0].text;
         } else {
-            throw new Error("AAAAAAAAAAAAAAA");
+            throw new Error(`Bad response: ${response.status}`);
         }
     }).catch((error) => {
         console.error(error);
-
     });
-    
-    return response;
   };
 
 const onSubmit = async () => {
@@ -28,7 +25,7 @@ const onSubmit = async () => {
     const sourceLang = document.getElementById('source-language').value;
     const targetLang = document.getElementById('target-language').value;
     const result = await getTranslation(authKey, input, sourceLang, targetLang);
-    console.log(result);
+    document.getElementById('output').value = result;
 };
 
 submitButton.addEventListener('click', onSubmit);
